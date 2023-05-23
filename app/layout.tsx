a6d25701b9053fa431daa29ad04b142124034c16
Raheel Ahmed <raheelahmed017@gmail.com>
2023-05-23 18:29:21 +0400

    second

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,31 @@
 import React from "react";
 
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import ToasterContext from "@/app/context/ToasterContext";
+import AuthContext from "@/app/context/AuthContext";
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
-  title: 'Messenger Clone',
-  description: 'Messenger Clone App',
+    title: 'Messenger Clone',
+    description: 'Messenger Clone App',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
+                                       children,
+                                   }: {
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-      <ToasterContext/>
-      {children}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+        <body className={inter.className}>
+        <AuthContext>
+            <ToasterContext/>
+            {children}
+        </AuthContext>
+        </body>
+        </html>
+    )
 }
